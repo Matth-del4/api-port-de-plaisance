a1172aa0278ef8827b8ce45f95866a6a6ca2c114
MatthieuAQB <mattaqb@gmail.com>
2026-02-16 10:03:00 +0100

    3 modèles créés user, catway, reservation

---
+++ Users/MattA/OneDrive/Desktop/port-plaisance-api/models/catway.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const catwaySchema = new mongoose.Schema(
+  {
+    catwayNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    catwayType: {
+      type: String,
+      required: true,
+      enum: ["long", "short"],
+    },
+    catwayState: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Catway", catwaySchema);
