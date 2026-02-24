b834539276df3b67a74fe81980a28240624f3157
MatthieuAQB <mattaqb@gmail.com>
2026-02-24 08:48:19 +0100

    Page d'accueil fonctionnel

--- Users/MattA/OneDrive/Desktop/port-plaisance-api/server.js
+++ Users/MattA/OneDrive/Desktop/port-plaisance-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.static("views"));
 
 const catwayRoutes = require("./routes/catways");
 const reservationRoutes = require("./routes/reservations");
